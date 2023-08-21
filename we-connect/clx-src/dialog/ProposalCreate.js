ea23e546498cc3cabdf79d57649eca2b829719a0
nayeon <keemnayn@gmail.com>
2023-08-21 17:37:57 +0900

    fix proposalCreate, proposalDetail 글쓰기와 상세보기

--- we-connect/clx-src/dialog/ProposalCreate.js
+++ we-connect/clx-src/dialog/ProposalCreate.js
@@ -5,3 +5,38 @@
  * @author keemn
  ************************************************/
 
+/*
+ * "등록" 버튼(btnInsert)에서 click 이벤트 발생 시 호출.
+ * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
+ */
+function onBtnInsertClick(e) {
+	var btnInsert = e.control;
+	var submission = app.lookup("proposalCreateSub");
+	var proposalTitle = app.lookup("proposalTitleIpb").value;
+	var proposalContent = app.lookup("proposalContentIpb").value;
+	if (!proposalTitle || !proposalContent) {
+		alert("건의글의 제목과 내용을 모두 입력해주세요.");
+	} else {
+		submission.send();
+	}
+}
+
+/*
+ * 서브미션에서 submit-success 이벤트 발생 시 호출.
+ * 통신이 성공하면 발생합니다.
+ */
+function onProposalCreateSubSubmitSuccess(e) {
+	var proposalCreateSub = e.control;
+	alert("건의사항 게시물 등록 완료");
+	app.close();
+}
+
+/*
+ * "취소" 버튼(btnRevert)에서 click 이벤트 발생 시 호출.
+ * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
+ */
+function onBtnRevertClick(e){
+	var btnRevert = e.control;
+	app.close();
+}
+
--- we-connect/src/main/resources/static/weconnect/member/ProposalDetail.clx.js
+++ we-connect/src/main/resources/static/weconnect/member/ProposalDetail.clx.js
@@ -16,7 +16,38 @@
 			 * Created at 2023. 8. 21. 오후 5:18:05.
 			 *
 			 * @author keemn
-			 ************************************************/;
+			 ************************************************/
+
+			/*
+			 * 루트 컨테이너에서 init 이벤트 발생 시 호출.
+			 * 앱이 최초 구성될 때 발생하는 이벤트 입니다.
+			 */
+			function onBodyInit(e) {
+				app.lookup("proposalDetailSub").send();
+				app.lookup("proposalCommentSub").send();
+				
+			}
+
+			/*
+			 * 서브미션에서 submit-success 이벤트 발생 시 호출.
+			 * 통신이 성공하면 발생합니다.
+			 */
+			function onProposalDetailSubSubmitSuccess(e) {
+				var proposalDetailSub = e.control;
+				app.lookup("proposalTitle").redraw();
+				app.lookup("proposalContent").redraw();
+				app.lookup("proposalCreate").redraw();
+				app.lookup("proposalStatus").redraw();
+			}
+
+			/*
+			 * 루트 컨테이너에서 load 이벤트 발생 시 호출.
+			 * 앱이 최초 구성된후 최초 랜더링 직후에 발생하는 이벤트 입니다.
+			 */
+			function onBodyLoad(e) {
+				app.lookup("proposalDetailSub").send();
+				app.lookup("proposalCommentSub").send();
+			};
 			// End - User Script
 			
 			// Header
