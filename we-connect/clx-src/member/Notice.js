5df49d0cba8d49724eed878b8a014831fd29a961
choi9818 <chwechoi9818@naver.com>
2023-08-25 09:31:33 +0900

    feat member 화면 공지사항 추가

--- we-connect/clx-src/member/Notice.js
+++ we-connect/clx-src/member/Notice.js
@@ -5,3 +5,10 @@
  * @author Axl Rose
  ************************************************/
 
+/*
+ * 루트 컨테이너에서 init 이벤트 발생 시 호출.
+ * 앱이 최초 구성될 때 발생하는 이벤트 입니다.
+ */
+function onBodyInit(e){
+	app.lookup("noticeListSub").send();
+}
--- we-connect/src/main/java/com/arezip/weconnect/service/NoticeServiceImpl.java
+++ we-connect/src/main/java/com/arezip/weconnect/service/NoticeServiceImpl.java
@@ -1,6 +1,11 @@
 package com.arezip.weconnect.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.arezip.weconnect.mapper.NoticeMapper;
+import com.arezip.weconnect.model.dto.NoticeDTO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,5 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class NoticeServiceImpl implements NoticeService {
+	private final NoticeMapper noticeMapper;
+	
+	//공지사항 전체 조회
+	@Override
+	public List<NoticeDTO> getNoticeList() {
+		return noticeMapper.getNoticeList();
+	}
 
 }
